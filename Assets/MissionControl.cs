abf04a83987162cc11dade164ef9407a8364804c
Jaein Park <jaynap@g.skku.edu>
2022-10-23 02:06:52 +0900

    fix quest ui bug

--- Assets/MissionControl.cs
+++ Assets/MissionControl.cs
@@ -9,40 +9,51 @@ public class MissionControl : MonoBehaviour
     public Transform content;
     public GameObject prefabMission;
     public int totalIndex = 0;
-    
-    
 
-   //content.getchild(0~3).gameObject -> mission1
+
+
+    //content.getchild(0~3).gameObject -> mission1
     // private Queue<GameObject> missions;
 
     // Start is called before the first frame update
-    public void SetMissionName(int n, string newName){
+    public void SetMissionName(int n, string newName)
+    {
 
     }
-    public void AddMission(string name){
+    public void AddMission(string name)
+    {
         //content.GetChild(0).gameObject.GetComponentsInChildren<TMP_Text>()[1].text = name;
         GameObject newMission = Instantiate<GameObject>(prefabMission, content);
-        newMission.gameObject.GetComponentsInChildren<TMP_Text>()[1].text = name+totalIndex;
+        newMission.gameObject.GetComponentsInChildren<TMP_Text>()[1].text = name + totalIndex;
         ChangeMissionColor();
-        totalIndex+=1;
+        totalIndex += 1;
     }
-    public void DeleteMission(){
+    public void DeleteMission()
+    {
+        if (content.childCount > 1)
+        {
+            Debug.Log("www");
+            content.GetChild(1).GetComponent<Image>().color = Color.white;
+        }
         Destroy(content.GetChild(0).gameObject);
         totalIndex = totalIndex - 1;
-        ChangeMissionColor();
-        
+
+        // ChangeMissionColor();
+        // content.GetChild(0).GetComponent<Image>().color = Color.white;
+
     }
-    private void ChangeMissionColor(){
-        
-        for(int i = 0;i<=totalIndex;i++)
+    private void ChangeMissionColor()
+    {
+        Debug.Log(content.GetChild(0).GetComponent<Image>().color);
+        content.GetChild(0).GetComponent<Image>().color = Color.white;
+        Debug.Log(content.GetChild(0).GetComponent<Image>().color);
+        for (int i = 1; i <= totalIndex; i++)
         {
-            if(i==0){
-                content.GetChild(i).gameObject.GetComponent<Image>().color = new Color(255f,255f,255f);
-            }
-            else content.GetChild(i).gameObject.GetComponent<Image>().color = new Color(56 / 255f, 56 / 255f, 56 / 255f);
+            content.GetChild(i).GetComponent<Image>().color = new Color(56 / 255f, 56 / 255f, 56 / 255f);
         }
     }
-    private void RearrangeMission(){
+    private void RearrangeMission()
+    {
         for (int i = 0; i < totalIndex; i++)
         {
             //content.GetChild(i) = content.GetChild(i+1);
@@ -56,6 +67,6 @@ public class MissionControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
