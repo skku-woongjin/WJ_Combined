783b6213b957ff82505120ed5c362f4649d47cf9
Jaein Park <jaynap@g.skku.edu>
2022-10-23 00:08:22 +0900

    quest npc

--- Assets/QuestNpcArea.cs
+++ Assets/QuestNpcArea.cs
@@ -7,19 +7,21 @@ public class QuestNpcArea : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             transform.parent.GetComponent<SaySomething>().say("퀘스트가 나올거야!");
+            GameManager.Instance.questGiver = transform.parent.GetComponent<SaySomething>();
+            GameManager.Instance.recAgent.recommend();
             transform.parent.rotation = Quaternion.LookRotation(other.transform.GetComponentInChildren<Camera>().transform.position - transform.parent.position);
         }
     }
