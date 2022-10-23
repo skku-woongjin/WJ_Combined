76ca09b84ad0088e37b54f052dadb323fc585b38
kang junemo <103883786+peter0107@users.noreply.github.com>
2022-10-23 13:49:13 +0900

    solar dialogflow connect

--- Assets/Assets/ML-Agents/Examples/FoodCollector/Scripts/Talking_NPC_Area.cs
+++ Assets/Assets/ML-Agents/Examples/FoodCollector/Scripts/Talking_NPC_Area.cs
@@ -6,8 +6,10 @@ public class Talking_NPC_Area : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject TTS_Audio;
+    public GameObject dialogflow;
     public bool Jurassic;
     public bool Solar;
+    bool In_Area=false;
     public GameObject chatmanager;
     
     void Start()
@@ -18,8 +20,15 @@ public class Talking_NPC_Area : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //NPC 영역안에 있고 유저가 텍스트를 전송했을 경우
+        if(In_Area && GameManager.Instance.userText_set){
+            Debug.Log("유저 솔라 영역안에 있음");
+            dialogflow.GetComponent<diag>().solar_start(GameManager.Instance.userText);
+            GameManager.Instance.userText_set=false;
+        }
     }
+
+
     string text;
     private void OnTriggerEnter(Collider other)
     {
@@ -32,15 +41,19 @@ public class Talking_NPC_Area : MonoBehaviour
             else if(Jurassic){
                 text="나는 쥬라기로봇 다이노야. 어떤 주제에 대해 알고싶니?";
             }
-            chatmanager.GetComponent<ChatManager>().Chat(false,text,"");
-            
-            TTS_Audio.GetComponent<TTS>().setText(text);
-            transform.parent.GetComponent<SaySomething>().say(text);
+            NPC_Talking(text);
+            In_Area=true;
             
             
         }
         }
     
+
+    public void NPC_Talking(string text){
+        chatmanager.GetComponent<ChatManager>().Chat(false,text,"");
+        TTS_Audio.GetComponent<TTS>().setText(text);
+        transform.parent.GetComponent<SaySomething>().say(text);
+    }
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
